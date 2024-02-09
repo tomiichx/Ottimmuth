0404d7b4dfaa78fb06215603f308b8b17cf02101
Tomić <tomiichx@gmail.com>
2024-02-09 10:33:59 +0100

    Initial commit

---
+++ Ottimmuth.cs
@@ -0,0 +1,68 @@
+﻿#pragma warning disable IDE1006
+using System.Drawing;
+
+namespace Ottimmuth
+{
+    public class Ottimmuth
+    {
+        private readonly Bitmap newImage;
+
+        public Ottimmuth(Bitmap newImage)
+        {
+            this.newImage = newImage;
+        }
+
+        public void startProcessing()
+        {
+            int width = newImage.Width;
+            int height = newImage.Height;
+
+            Color[,] pixelColors = new Color[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    pixelColors[x, y] = newImage.GetPixel(x, y);
+                }
+            }
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (!isOttimmuth(x, y, pixelColors))
+                        continue;
+
+                    newImage.SetPixel(x, y, Color.White);
+                }
+            }
+        }
+
+        private bool isOttimmuth(int x, int y, Color[,] pixelColors)
+        {
+            Color currentPixel = pixelColors[x, y];
+
+            if (x > 0 && pixelColors[x - 1, y] == currentPixel)
+            {
+                return true;
+            }
+
+            if (x < newImage.Width - 1 && pixelColors[x + 1, y] == currentPixel)
+            {
+                return true;
+            }
+
+            if (y > 0 && pixelColors[x, y - 1] == currentPixel)
+            {
+                return true;
+            }
+
+            if (y < newImage.Height - 1 && pixelColors[x, y + 1] == currentPixel)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Ottimmuth
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+            string rootPath = executablePath.Substring(0, executablePath.IndexOf("bin"));
+
+            string[] localAssets = Directory.GetFiles(rootPath + "assets", "*.png");
+
+            if (localAssets.Length == 0)
+            {
+                Console.WriteLine("Es wurden keine Bilder gefunden.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Wählen Sie ein Bild aus, das Sie bearbeiten möchten: ");
+
+            for (int i = 0; i < localAssets.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + Path.GetFileName(localAssets[i]));
+                if (i == localAssets.Length - 1) Console.WriteLine("\n");
+            }
+
+            int selectedOption = int.Parse(Console.ReadLine()) - 1;
+
+            if (selectedOption < 0 || selectedOption >= localAssets.Length)
+            {
+                Console.WriteLine("Die ausgewählte Option ist ungültig.");
+                Console.ReadKey();
+                return;
+            }
+
+            if (File.Exists(rootPath + "assets/" + Path.GetFileNameWithoutExtension(localAssets[selectedOption]) + "_bearbeitet.png"))
+            {
+                Console.WriteLine("Das Bild wurde bereits bearbeitet, und befindet sich im assets/bearbeitet-Ordner. (" + Path.GetFileNameWithoutExtension(localAssets[selectedOption]) + "_bearbeitet.png" + ")");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("\nDas Bild wird bearbeitet...\n");
+
+            Bitmap newImage = new Bitmap(localAssets[selectedOption]);
+
+            if (!File.Exists(localAssets[selectedOption]))
+            {
+                Console.WriteLine("Das Bild konnte nicht geladen werden.");
+                Console.ReadKey();
+                return;
+            }
+
+            Ottimmuth ottimmuth = new Ottimmuth(newImage);
+
+            ottimmuth.startProcessing();
+
+            newImage.Save(rootPath + "assets/bearbeitet/" + Path.GetFileNameWithoutExtension(localAssets[selectedOption]) + "_bearbeitet.png");
+            newImage.Dispose();
+
+            Console.WriteLine("Das Bild wurde erfolgreich bearbeitet, und befindet sich im assets/bearbeitet-Ordner. (" + Path.GetFileNameWithoutExtension(localAssets[selectedOption]) + "_bearbeitet.png" + ")");
+            Console.ReadKey();
+        }
+    }
+}
